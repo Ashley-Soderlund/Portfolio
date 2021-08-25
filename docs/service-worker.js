b38d69303c14ef9502035f75a5ffb8b60ae7382c
Ashley Soderlund <Ashley.Soderlund@sckans.edu>
2021-08-24 20:45:12 -0500

    edit

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Ashley-Soderlund/Portfolio/precache-manifest.539ee987a033676f206a9903ccb049c8.js"
+  "/Portfolio/precache-manifest.be961c47f8fefcc8f3559b04c19f3ff9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Ashley-Soderlund/Portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
